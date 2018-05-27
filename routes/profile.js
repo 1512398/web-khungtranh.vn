fc31b7f717e432cfb90316493a8cb235f57b1585
HoangTrungHieu <hoangtrunghieu@HoangTrungHieus-MacBook-Pro.local>
2018-05-27 15:43:26 +0700

    remove temp pic

--- routes/profile.js
+++ routes/profile.js
@@ -15,35 +15,6 @@ router.get('/', isLoggedIn, function (req, res) {
     var avtPath =req.session.avtImg;
     res.render('CapNhatThongTin', {imgPath:avtPath, user: req.user, message: req.flash('updateProfileMessage'), messageSuccess: req.flash('updateSuccessful') });
 })
-
-router.post('/update', isLoggedIn, function (req, res) {
-    console.log(req.body);
-    console.log(req.data)
-    var new_pass = '';
-    if (req.body.new_password == '') // neu khong co yeu cau thay doi mat khau
-        new_pass = req.user.password
-    else // neu co yeu cau thay doi mat khau
-    {
-        if (isValidPassword(req.user.password, req.body.password)) // kiem tra password hien tai co khop voi password cu
-            new_pass = bCrypt.hashSync(req.body.new_password, bCrypt.genSaltSync(8), null);  // neu trung khop, chuan bi update DB
-        else
-            {
-                req.flash('updateProfileMessage', 'Cập nhật không thành công - Mật khẩu không đúng'); // Thong bao loi va ket thuc
-                res.redirect('/CapNhatThongTin');
-                return;
-            }
-    }
-
-    userController.edit(
-        req.user.email,
-        new_pass,
-        req.body.address,
-        req.body.phoneNum,
-        function (user) {
-            req.flash('updateSuccessful', 'Các thay đổi đã được cập nhật thành công !');
-            res.redirect('/CapNhatThongTin');
-        });
-})
 // Set storage engine
 const multer = require('multer');
 const path = require('path')
@@ -57,8 +28,58 @@ const storage = multer.diskStorage({
 //Init Upload
 const upload = multer({
 	storage: storage,
-	limits:{fileSize:100000000000}
+	limits:{fileSize:100000000}
 }).single('myImg');
+
+router.post('/update', 
+    isLoggedIn, 
+    function(req,res,next){
+        console.log(req.body)
+        upload(req, res, function(err){
+            if ((err)||(req.file === undefined)) {
+                console.log('avt faileddd')
+                // res.redirect('/CapNhatThongTin');
+            }
+            else{
+                console.log('file: ',req.file)
+                req.session.avtImg = String(req.file.path).replace('public','');
+                // res.redirect('/CapNhatThongTin');   
+            }
+        })
+        console.log('oke');
+        return next();
+    },
+    function (req, res) {
+        console.log('oke2');
+    var new_pass = '';
+    if (req.body.new_password == '') // neu khong co yeu cau thay doi mat khau
+        new_pass = req.user.password
+    else // neu co yeu cau thay doi mat khau
+    {
+        console.log('oke3')
+        console.log(req.new_password)
+        console.log(req.user.password, req.body.phoneNum);
+        if (isValidPassword(req.user.password, req.body.password)) // kiem tra password hien tai co khop voi password cu
+            new_pass = bCrypt.hashSync(req.body.new_password, bCrypt.genSaltSync(8), null);  // neu trung khop, chuan bi update DB
+        else
+            {
+                req.flash('updateProfileMessage', 'Cập nhật không thành công - Mật khẩu không đúng'); // Thong bao loi va ket thuc
+                res.redirect('/CapNhatThongTin');
+                return;
+            }
+    }
+    console.log('oke4')
+    userController.edit(
+        req.user.email,
+        new_pass,
+        req.body.address,
+        req.body.phoneNum,
+        function (user) {
+            req.flash('updateSuccessful', 'Các thay đổi đã được cập nhật thành công !');
+            res.redirect('/CapNhatThongTin');
+        });
+})
+
 
 router.post('/uploadAvt', function (req,res) {
 	upload(req, res, function(err){
